148547f862d2e09824e43ad54abfd4a685be6f0c
Mexil77 <mendel_77@hotmail.com>
2025-06-19 12:27:29 +0200

    add first component

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,23 +1,11 @@
-const app = document.getElementById("app");
+import { Navbar } from "./components/index.js";
+
+document.addEventListener("DOMContentLoader", () => {});
 const div = document.createElement("div");
 const card = document.createElement("button");
 card.className =
 	"card w-64 h-80 bg-gray-800 text-white p-6 rounded-lg shadow-lg relative overflow-hidden transition-transform duration-300 hover:scale-105";
 card.textContent = "click dinamico";
-card.addEventListener("click", () => {
-	fetch("http://localhost:3000/ping")
-		.then((response) => {
-			if (!response.ok) {
-				throw new Error(`Error en la respuesta: ${response.status}`);
-			}
-			return response.text();
-		})
-		.then((data) => {
-			console.log("Respuesta del servidor:", data);
-		})
-		.catch((error) => {
-			console.error("Hubo un problema con el fetch:", error);
-		});
-});
 div.appendChild(card);
-document.body.insertBefore(div, app);
+const app = document.getElementById("app");
+document.body.insertBefore(Navbar(), app);
